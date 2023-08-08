518d755a904e195e36df1644d88acecec85fc9b1
chxxah <chxxah@localhost>
2023-08-08 20:00:26 +0900

    [2023-08-08]

---
+++ jsp/src/main/java/com/peazh/web/BoardDAO.java
@@ -0,0 +1,10 @@
+package com.peazh.web;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface BoardDAO {
+	public Integer toSize();
+}
---
+++ jsp/src/main/java/com/peazh/web/BoardService.java
@@ -0,0 +1,18 @@
+package com.peazh.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+	@Autowired
+	private BoardDAO boardDAO;
+	
+	public int toSize() {
+		return boardDAO.toSize();
+	}
+	
+	
+
+}
